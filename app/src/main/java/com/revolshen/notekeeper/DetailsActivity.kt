b7164835d72079cae0f2a726645ae79438d1fc37
Rejfi <rrafaul@wp.pl>
2019-05-29 23:52:28 +0200

    Add save button and ActionBar

--- app/src/main/java/com/revolshen/notekeeper/DetailsActivity.kt
+++ app/src/main/java/com/revolshen/notekeeper/DetailsActivity.kt
@@ -1,11 +1,33 @@
 package com.revolshen.notekeeper
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 
 class DetailsActivity: AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.save_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if(item?.itemId == R.id.saveBT_details){
+            val dbHelper = SQLDataBaseHelper(applicationContext)
+            val db = dbHelper.writableDatabase
+
+
+            onBackPressed()
+
+
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
